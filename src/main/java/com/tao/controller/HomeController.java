8d96940cca91044e359142a82abccda0b3dded9f
LiuRentao <liurentaoliu@foxmail.com>
2025-08-27 17:48:21 +0800

    初始化项目

--- src/main/java/com/tao/controller/HomeController.java
+++ src/main/java/com/tao/controller/HomeController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @ClassName HomeController
- * @Description TODO
- * @Author LiuRentao
- * @Since 2025/8/27 17:23
- * @Version 1.0
+ * HomeController
+ * @author LiuRentao
+ * @since 2025/8/27 17:23
+ * @version 1.0
  */
 @RestController
 public class HomeController {
--- src/main/java/com/tao/controller/PageController.java
+++ src/main/java/com/tao/controller/PageController.java
@@ -1,20 +1,13 @@
 package com.tao.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * @ClassName PageController
- * @Description TODO
- * @Author LiuRentao
- * @Since 2025/8/27 17:26
- * @Version 1.0
+ * PageController
+ * @author LiuRentao
+ * @since 2025/8/27 17:47
+ * @version 1.0
  */
 @Controller
 public class PageController {
-
-    @GetMapping("/page")
-    public String home() {
-        return "/page/home.html";
-    }
 }
